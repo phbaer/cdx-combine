1d7391388c16c40686d5dc2f034c9618ad955f5e
Philipp A. Baer <github@ph.baer.one>
2024-10-11 05:49:11 +0200

    Fix formatting, using blake, isort, and autoflake

--- src/cdx_combine/__main__.py
+++ src/cdx_combine/__main__.py
@@ -1,28 +1,29 @@
-from cdx_combine import CDX_Combine
 import argparse
 import logging
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-                    prog='cdx_combine',
-                    description='Combines multiple CycloneDX SBOM files into one. It will keep the component names.')
-    parser.add_argument('files', nargs='*')
-    parser.add_argument('-o', '--output', default='cyclonedx.json')
-    parser.add_argument('-n', '--name', required=True)
-    parser.add_argument('-v', '--version', required=True)
-    parser.add_argument(      '--verbose', action="store_true")
+from cdx_combine import CDX_Combine
 
-    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="cdx_combine",
+        description="Combines multiple CycloneDX SBOM files into one. It will keep the component names.",
+    )
+    parser.add_argument("files", nargs="*")
+    parser.add_argument("-o", "--output", default="cyclonedx.json")
+    parser.add_argument("-n", "--name", required=True)
+    parser.add_argument("-v", "--version", required=True)
+    parser.add_argument("--verbose", action="store_true")
+
     try:
         args = parser.parse_args()
 
         logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
-        logger = logging.getLogger('main')
-        
-        if 'files' not in args or args.files is None or len(args.files) == 0:
-            logger.error(f'At least one input file required!')
+        logger = logging.getLogger("main")
+
+        if "files" not in args or args.files is None or len(args.files) == 0:
+            logger.error("At least one input file required!")
             exit(1)
-            
+
         CDX_Combine().run(args)
-    except SystemExit as e:
+    except SystemExit:
         pass
--- src/cdx_combine/cdx_combine.py
+++ src/cdx_combine/cdx_combine.py
@@ -1,4 +1,8 @@
+import glob
 import json
+import logging
+
+from cyclonedx.exception import MissingOptionalDependencyException
 from cyclonedx.model.bom import Bom
 from cyclonedx.model.bom_ref import BomRef
 from cyclonedx.model.component import Component, ComponentType
@@ -6,15 +10,12 @@ from cyclonedx.model.dependency import Dependency
 from cyclonedx.output.json import JsonV1Dot5
 from cyclonedx.schema import SchemaVersion
 from cyclonedx.validation.json import JsonStrictValidator
-from cyclonedx.exception import MissingOptionalDependencyException
-import glob
-import logging
 
 
 class CDX_Combine:
 
     def __init__(self):
-        self._logger = logging.getLogger('cdxc')
+        self._logger = logging.getLogger("cdxc")
         self._new_bom_components = dict()
         self._new_bom_ref_map = dict()
 
@@ -40,88 +41,101 @@ class CDX_Combine:
             properties=component.properties,
             release_notes=component.release_notes,
             scope=component.scope,
-            swid=component.swid)
-    
+            swid=component.swid,
+        )
+
     def _get_ref_from_component(self, component: Component) -> str:
         if component.group:
-            return f'cdxc:{component.group}/{component.name}@{component.version}'
-        return f'cdxc:{component.name}@{component.version}'
-    
-    def _lookup_new_bom_ref(self, dependency: Dependency) -> str|None:
+            return f"cdxc:{component.group}/{component.name}@{component.version}"
+        return f"cdxc:{component.name}@{component.version}"
+
+    def _lookup_new_bom_ref(self, dependency: Dependency) -> str | None:
         if str(dependency.ref) not in self._new_bom_ref_map:
-            self._logger.error(f'BOM reference {dependency.ref} unknown!')
+            self._logger.error("BOM reference %s unknown!", dependency.ref)
             return None
         return self._new_bom_ref_map[str(dependency.ref)]
-    
+
     def _lookup_component(self, dependency: Dependency) -> Component:
         new_bom_ref = self._lookup_new_bom_ref(dependency)
         if new_bom_ref not in self._new_bom_components:
-            self._logger.error(f'BOM reference {new_bom_ref} not found!')
+            self._logger.error("BOM reference %s not found!", new_bom_ref)
             return None
         return self._new_bom_components[new_bom_ref]
 
-    def _simplify_ref(self, bom_ref: str|BomRef) -> str:
+    def _simplify_ref(self, bom_ref: str | BomRef) -> str:
         bom_ref_str = old_bom_ref_str = str(bom_ref)
-        if '|' in bom_ref_str:
-            bom_ref_str = bom_ref_str.split('|').pop()
-            self._logger.debug(f'Mapping {old_bom_ref_str} onto {bom_ref_str}')
+        if "|" in bom_ref_str:
+            bom_ref_str = bom_ref_str.split("|").pop()
+            self._logger.debug("Mapping %s onto %s", old_bom_ref_str, bom_ref_str)
         return bom_ref_str
 
     def _add_components(self, new_bom: Bom, component: Component):
         bom_ref_str = self._get_ref_from_component(component)
         bom_ref_orig_str = str(component.bom_ref)
         if bom_ref_str not in self._new_bom_components:
-            self._logger.debug(f'Adding component {bom_ref_str}')
+            self._logger.debug("Adding component %s", bom_ref_str)
             new_component = self._clone_component(bom_ref_str, component)
             new_bom.components.add(new_component)
             self._new_bom_components[bom_ref_str] = new_component
-        
+
         self._new_bom_ref_map[bom_ref_orig_str] = bom_ref_str
         for subcomponent in component.components:
             self._add_components(new_bom, subcomponent)
 
     def _mirror_dependencies(self, new_bom: Bom, dependency: Dependency):
-        self._logger.debug(f'Processing dependecy {dependency.ref}')
+        self._logger.debug("Processing dependecy %s", dependency.ref)
         dependency_component = self._lookup_component(dependency)
         if dependency_component is not None:
             for subdependency in dependency.dependencies:
                 subdependency_component = self._lookup_component(subdependency)
                 if subdependency_component is not None:
-                    new_bom.register_dependency(dependency_component, [subdependency_component])
+                    new_bom.register_dependency(
+                        dependency_component, [subdependency_component]
+                    )
                     self._mirror_dependencies(new_bom, subdependency)
 
     def run(self, args):
         name: str = args.name
         version: str = args.version
-        ref = name.lower().replace(' ', '_')
+        ref = name.lower().replace(" ", "_")
         files = args.files
 
         self._logger.setLevel(logging.DEBUG if args.verbose else logging.INFO)
-        
-        self._logger.info(f'Generating SBOM for {name} {version} from {", ".join(files)}...')
+
+        self._logger.info(
+            "Generating SBOM for %s %s from %s...", name, version, ", ".join(files)
+        )
 
         boms = []
         for file in files:
             for file in glob.glob(file):
-                logging.info(f'Loading {file}...')
-                with open(file, 'r', encoding='utf-8') as input_file:
+                logging.info("Loading %s...", file)
+                with open(file, "r", encoding="utf-8") as input_file:
                     boms.append(Bom.from_json(data=json.loads(input_file.read())))
 
         new_bom = Bom()
-        new_bom.metadata.component = root_component = Component(name=name, type=ComponentType.APPLICATION, licenses=[], bom_ref=ref, version=version)
+        new_bom.metadata.component = root_component = Component(
+            name=name,
+            type=ComponentType.APPLICATION,
+            licenses=[],
+            bom_ref=ref,
+            version=version,
+        )
 
         for bom in boms:
             bom_ref_str_orig = bom.metadata.component.bom_ref.value
             bom_ref_str = self._get_ref_from_component(bom.metadata.component)
             bom_component = self._clone_component(bom_ref_str, bom.metadata.component)
-            self._logger.info(f'Processing {bom_ref_str_orig}...')
+            self._logger.info("Processing %s...", bom_ref_str_orig)
 
             new_bom_ref = self._get_ref_from_component(bom_component)
             bom_component.bom_ref.value = new_bom_ref
 
             new_bom.components.add(bom_component)
             self._new_bom_components[bom_ref_str] = bom_component
-            self._new_bom_ref_map[bom_ref_str_orig] = self._get_ref_from_component(bom_component)
+            self._new_bom_ref_map[bom_ref_str_orig] = self._get_ref_from_component(
+                bom_component
+            )
 
             for component in bom.components:
                 self._add_components(new_bom, component)
@@ -131,21 +145,26 @@ class CDX_Combine:
 
             new_bom.register_dependency(root_component, [bom_component])
 
-        self._logger.info(f'New SBOM {args.output} contains {len(self._new_bom_components)} components')
+        self._logger.info(
+            "New SBOM %s contains %d components",
+            args.output,
+            len(self._new_bom_components),
+        )
 
-        my_json_outputter: 'JsonOutputter' = JsonV1Dot5(new_bom)
+        my_json_outputter: "JsonOutputter" = JsonV1Dot5(new_bom)
         serialized_json = my_json_outputter.output_as_string(indent=2)
 
         my_json_validator = JsonStrictValidator(SchemaVersion.V1_5)
         try:
             validation_errors = my_json_validator.validate_str(serialized_json)
             if validation_errors:
-                self._logger.error(f'JSON invalid:\nValidationError:\n{repr(validation_errors)}')
-                #sys.exit(2)
-            #print('JSON valid')
+                self._logger.error(
+                    "JSON invalid:\nValidationError:\n%s", repr(validation_errors)
+                )
+                # sys.exit(2)
+            # print('JSON valid')
         except MissingOptionalDependencyException as error:
-            self._logger.error(f'JSON-validation was skipped due to {error}')
-            
-        with open(args.output, 'w') as f:
-            f.write(serialized_json)
+            self._logger.error("JSON-validation was skipped due to %s", error)
 
+        with open(args.output, "w", encoding="UTF-8") as f:
+            f.write(serialized_json)
